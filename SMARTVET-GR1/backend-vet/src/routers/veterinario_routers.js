8365e58f68efa353685307bf1aba02e739e36535
MarioEndara <150484870+MarioEndara@users.noreply.github.com>
2025-06-16 15:12:27 -0500

    Delete SMARTVET-GR1 directory

--- SMARTVET-GR1/backend-vet/src/routers/veterinario_routers.js
+++
@@ -1,6 +0,0 @@
-import { Router }    from "express";
-import { registro } from "../controllers/veterinario_controle.js";
- const router = Router()
- router .post('/registro',registro)
-
- export default router
